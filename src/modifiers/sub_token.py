740987dfaed5223bdd7717a5a234509f0a85546e
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-07-17 01:27:37 +0200

    Added first/last range subtoken

--- src/modifiers/sub_token.py
+++ src/modifiers/sub_token.py
@@ -7,8 +7,11 @@ ctx.matches = r"""
 tag: user.cursorless
 """
 
+mod.list("cursorless_subtoken", desc="Supported subcomponent types")
+ctx.lists["self.cursorless_subtoken"] = {"word": "word", "char": "character"}
 
-@mod.capture(rule=("<user.ordinals> | last"))
+
+@mod.capture(rule="<user.ordinals> | last")
 def ordinal_or_last(m) -> str:
     """An ordinal or the word 'last'"""
     if m[0] == "last":
@@ -16,23 +19,35 @@ def ordinal_or_last(m) -> str:
     return m.ordinals - 1
 
 
-mod.list("cursorless_subtoken", desc="Supported subcomponent types")
+@mod.capture(rule="<user.ordinal_or_last> [past <user.ordinal_or_last>]")
+def cursorless_ordinal_range(m) -> str:
+    """Ordinal range"""
+    return {
+        "startIndex": m.ordinal_or_last_list[0],
+        "endIndex": m.ordinal_or_last_list[-1] + 1 or None,
+    }
 
-ctx.lists["self.cursorless_subtoken"] = {"word": "word", "char": "character"}
+
+@mod.capture(rule="(first | last) <number_small>")
+def cursorless_first_last_range(m) -> str:
+    """First/last range"""
+    if m[0] == "first":
+        return {"startIndex": 0, "endIndex": m.number_small}
+    return {"startIndex": -m.number_small, "endIndex": -1}
 
 
 @mod.capture(
     rule=(
-        "<user.ordinal_or_last> [past <user.ordinal_or_last>] {user.cursorless_subtoken}"
+        "(<user.cursorless_ordinal_range> | <user.cursorless_first_last_range>)"
+        "{user.cursorless_subtoken}"
     )
 )
 def cursorless_subtoken(m) -> str:
     """Subtoken ranges such as subwords or characters"""
+    try:
+        range = m.cursorless_ordinal_range
+    except AttributeError:
+        range = m.cursorless_first_last_range
     return {
-        "modifier": {
-            "type": "subpiece",
-            "pieceType": m.cursorless_subtoken,
-            "startIndex": m.ordinal_or_last_list[0],
-            "endIndex": m.ordinal_or_last_list[-1] + 1 or None,
-        }
+        "modifier": {"type": "subpiece", "pieceType": m.cursorless_subtoken, **range}
     }
