30138004505e305c7a79acc368afd2b255f4833e
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-07-21 22:01:54 +0200

    Added range modifiers till and tween

--- src/compound_targets.py
+++ src/compound_targets.py
@@ -7,31 +7,31 @@ mod = Module()
 @mod.capture(
     rule=(
         "<user.cursorless_primitive_target> | "
-        "past <user.cursorless_primitive_target> | "
-        "<user.cursorless_primitive_target> past <user.cursorless_primitive_target>"
+        "(past|till|tween) <user.cursorless_primitive_target> | "
+        "<user.cursorless_primitive_target> (past|till|tween) <user.cursorless_primitive_target>"
     )
 )
 def cursorless_range(m) -> str:
-    if "past" in m:
-        end = m[-1]
-        if m[0] == "past":
-            start = BASE_TARGET.copy()
-        else:
-            start = m.cursorless_primitive_target_list[0]
-        return {
-            "type": "range",
-            "start": start,
-            "end": end,
-        }
+    length = len(m)
+    if length == 1:
+        return m[0]
 
-    return m[0]
+    if length == 2:
+        start = BASE_TARGET.copy()
+    else:
+        start = m[0]
+    modifier = m[-2]
+    return {
+        "type": "range",
+        "start": start,
+        "end": m[-1],
+        "excludeStart": modifier == "tween",
+        "excludeEnd": modifier in ["tween", "till"],
+    }
 
 
 @mod.capture(rule=("<user.cursorless_range> (and <user.cursorless_range>)*"))
 def cursorless_target(m) -> str:
     if len(m.cursorless_range_list) == 1:
         return m.cursorless_range
-    return {
-        "type": "list",
-        "elements": m.cursorless_range_list
-    }
+    return {"type": "list", "elements": m.cursorless_range_list}
