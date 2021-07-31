f4230e1c9ff2c8e68cb313eceb1b12743fc8c0df
Maciej Klimek <maciej.klimek@gmail.com>
2021-07-31 14:18:45 +0200

    Add surrouding_pair modifier

--- src/modifiers/surrounding_pair.py
+++ src/modifiers/surrounding_pair.py
@@ -21,7 +21,22 @@ mod.list("cursorless_pair_symbol", desc="A symbol that comes in pairs, eg bracke
 ctx.lists["self.cursorless_pair_symbol"] = pair_symbols
 
 
-@mod.capture(rule=("{user.cursorless_pair_symbol}"))
+@mod.capture(
+    rule=(
+        "matching | {user.cursorless_pair_symbol} | pair [{user.cursorless_pair_symbol}] "
+    )
+)
 def cursorless_surrounding_pair(m) -> str:
     """Surrounding pair modifiers"""
-    return {"modifier": {"type": "surroundingPair", "delimiter": m.pair_symbol}}
+    try:
+        cursorless_pair_symbol = m.cursorless_pair_symbol
+    except AttributeError:
+        cursorless_pair_symbol = None
+
+    return {
+        "modifier": {
+            "type": "surroundingPair",
+            "delimeter": cursorless_pair_symbol,
+            "delimetersOnly": m[0] == "pair",
+        }
+    }
