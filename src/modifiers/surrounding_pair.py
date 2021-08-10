472accfa5f8509f5df3b89ab396d8468f3d8bdaf
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-08-10 14:36:24 +0200

    Require matching prefix

--- src/modifiers/surrounding_pair.py
+++ src/modifiers/surrounding_pair.py
@@ -19,11 +19,7 @@ ctx.lists["self.cursorless_pair_symbol"] = {
 }
 
 
-@mod.capture(
-    rule=(
-        "matching | {user.cursorless_pair_symbol} | pair [{user.cursorless_pair_symbol}] "
-    )
-)
+@mod.capture(rule=("(matching | pair) [{user.cursorless_pair_symbol}]"))
 def cursorless_surrounding_pair(m) -> str:
     """Surrounding pair modifier"""
     try:
