079915b54408d0c630c74a5fca1e621fcce2e32d
Pokey Rule <pokey.rule@gmail.com>
2021-08-31 19:02:00 +0100

    Fix cheatsheet (#70)

--- src/cheat_sheet.py
+++ src/cheat_sheet.py
@@ -208,10 +208,6 @@ class CheatSheet:
         self.draw_items(canvas, get_list("hat_color"))
 
         self.next_row()
-        self.draw_header(canvas, "Shapes")
-        self.draw_items(canvas, get_list("hat_shape"))
-
-        self.next_row()
         self.draw_header(canvas, "Examples")
         self.draw_items(
             canvas,
