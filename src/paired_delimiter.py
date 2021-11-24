d516b8a3b13becf7812a4fe3b88fb1f806a5efe9
Pokey Rule <pokey.rule@gmail.com>
2021-11-24 17:39:08 +0000

    Cleanup

--- src/paired_delimiter.py
+++ src/paired_delimiter.py
@@ -7,15 +7,15 @@ mod.list(
     "cursorless_paired_delimiter", desc="A symbol that comes in pairs, eg brackets"
 )
 mod.list(
-    "cursorless_wrappable_only_paired_delimiter",
-    desc="A paired delimiter that can be used as a wrapper",
+    "cursorless_wrapper_only_paired_delimiter",
+    desc="A paired delimiter that can only be used as a wrapper",
 )
 mod.list(
     "cursorless_selectable_only_paired_delimiter",
-    desc="A paired delimiter that can be used as a scope type",
+    desc="A paired delimiter that can only be used as a scope type",
 )
 mod.list(
-    "cursorless_wrappable_selectable_paired_delimiter",
+    "cursorless_wrapper_selectable_paired_delimiter",
     desc="A paired delimiter that can be used as a scope type and as a wrapper",
 )
 
@@ -28,7 +28,7 @@ class PairedDelimiter:
     right: str
 
     # Indicates whether the delimiter can be used to wrap a target
-    is_wrappable: bool = True
+    is_wrapper: bool = True
 
     # Indicates whether the delimiter can be used for expanding to surrounding
     # pair.
@@ -49,63 +49,63 @@ paired_delimiters = [
     PairedDelimiter("void", "whitespace", " ", " ", is_selectable=False),
     PairedDelimiter("square", "squareBrackets", "[", "]"),
     PairedDelimiter("twin", "singleQuotes", "'", "'"),
-    PairedDelimiter("outside", "any", "", "", is_wrappable=False),
+    PairedDelimiter("outside", "any", "", "", is_wrapper=False),
 ]
 
 paired_delimiters_map = {term.cursorlessIdentifier: term for term in paired_delimiters}
 
-wrappable_paired_delimiters_defaults = {
+wrapper_paired_delimiters_defaults = {
     term.defaultSpokenForm: term.cursorlessIdentifier
     for term in paired_delimiters
-    if term.is_wrappable and not term.is_selectable
+    if term.is_wrapper and not term.is_selectable
 }
 
 selectable_paired_delimiters_defaults = {
     term.defaultSpokenForm: term.cursorlessIdentifier
     for term in paired_delimiters
-    if term.is_selectable and not term.is_wrappable
+    if term.is_selectable and not term.is_wrapper
 }
 
-wrappable_selectable_paired_delimiters_defaults = {
+wrapper_selectable_paired_delimiters_defaults = {
     term.defaultSpokenForm: term.cursorlessIdentifier
     for term in paired_delimiters
-    if term.is_selectable and term.is_wrappable
+    if term.is_selectable and term.is_wrapper
 }
 
 
 @mod.capture(
     rule=(
-        "{user.cursorless_wrappable_only_paired_delimiter} |"
-        "{user.cursorless_wrappable_selectable_paired_delimiter}"
+        "{user.cursorless_wrapper_only_paired_delimiter} |"
+        "{user.cursorless_wrapper_selectable_paired_delimiter}"
     )
 )
-def cursorless_wrappable_paired_delimiter(m) -> str:
+def cursorless_wrapper_paired_delimiter(m) -> str:
     try:
-        return m.cursorless_wrappable_only_paired_delimiter
+        return m.cursorless_wrapper_only_paired_delimiter
     except AttributeError:
-        return m.cursorless_wrappable_selectable_paired_delimiter
+        return m.cursorless_wrapper_selectable_paired_delimiter
 
 
 @mod.capture(
     rule=(
         "{user.cursorless_selectable_only_paired_delimiter} |"
-        "{user.cursorless_wrappable_selectable_paired_delimiter}"
+        "{user.cursorless_wrapper_selectable_paired_delimiter}"
     )
 )
 def cursorless_selectable_paired_delimiter(m) -> str:
     try:
         return m.cursorless_selectable_only_paired_delimiter
     except AttributeError:
-        return m.cursorless_wrappable_selectable_paired_delimiter
+        return m.cursorless_wrapper_selectable_paired_delimiter
 
 
 def on_ready():
     init_csv_and_watch_changes(
         "paired_delimiters",
         {
-            "wrappable_only_paired_delimiter": wrappable_paired_delimiters_defaults,
+            "wrapper_only_paired_delimiter": wrapper_paired_delimiters_defaults,
             "selectable_only_paired_delimiter": selectable_paired_delimiters_defaults,
-            "wrappable_selectable_paired_delimiter": wrappable_selectable_paired_delimiters_defaults,
+            "wrapper_selectable_paired_delimiter": wrapper_selectable_paired_delimiters_defaults,
         },
     )
 
