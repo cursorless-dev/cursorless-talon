1c7e0621dafd5d4d4dbc10646a50f98de3421c9c
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-12-01 14:20:29 +0100

    Added action remove selection (#110)
    * Added action remove selection
    
    * Renamed remove selection to deselect

--- src/actions/actions.py
+++ src/actions/actions.py
@@ -77,6 +77,7 @@ simple_actions = {
     "extract": "extractVariable",
     "float": "insertEmptyLineAfter",
     "fold": "foldRegion",
+    "give": "deselect",
     "indent": "indentLine",
     "paste to": "pasteFromClipboard",
     "post": "setSelectionAfter",
