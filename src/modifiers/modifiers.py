6acb3ffb6f3e17351cdc56ca638e3592ca724d68
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-12-02 18:56:06 +0100

    Added identity modifier. Added head tail modifier to csv (#115)
    * Added identity modifier. Added head tail modifier to csv
    
    * Update src/modifiers/identity.py
    
    Co-authored-by: Pokey Rule <pokey.rule@gmail.com>
    
    * Update src/modifiers/modifiers.py
    
    Co-authored-by: Pokey Rule <pokey.rule@gmail.com>
    
    * Changed identifier on head tail
    
    * Update src/modifiers/identity.py
    
    Co-authored-by: Pokey Rule <pokey.rule@gmail.com>

--- src/modifiers/modifiers.py
+++ src/modifiers/modifiers.py
@@ -1,5 +1,6 @@
 from talon import app
 from ..csv_overrides import init_csv_and_watch_changes
+from .head_tail import head_tail
 
 # NOTE: Please do not change these dicts.  Use the CSVs for customization.
 # See https://github.com/pokey/cursorless-talon/blob/main/docs/customization.md
@@ -7,6 +8,7 @@ delimiter_inclusions = {
     "inside": "interiorOnly",
     "bound": "excludeInterior",
 }
+identity = {"just": "identity"}
 
 
 def on_ready():
@@ -14,6 +16,8 @@ def on_ready():
         "modifiers",
         {
             "delimiter_inclusion": delimiter_inclusions,
+            "head_tail": head_tail,
+            "identity": identity,
         },
     )
 
