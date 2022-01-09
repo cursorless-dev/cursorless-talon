cecf601c315bee5ad6cd04c34df03edfe3990847
Pokey Rule <pokey.rule@gmail.com>
2022-01-09 16:19:21 +0000

    Black

--- src/tutorial.py
+++ src/tutorial.py
@@ -3,18 +3,22 @@ import re
 
 regex = re.compile(r"\{(\w+):([^}]+)\}")
 
+
 def process_tutorial_step(raw: str):
     print([(match.group(1), match.group(2)) for match in regex.finditer(raw)])
     content = "whatever"
 
     return {
         "content": content,
-		"modes": ["command"],
-		"app": "vscode",		
-		"context_hint": "Please open VSCode",
+        "modes": ["command"],
+        "app": "vscode",
+        "context_hint": "Please open VSCode",
     }
 
-with open("/Users/pokey/src/cursorless-vscode/src/test/suite/fixtures/recorded/tutorial/unit-2-basic-coding/script.json") as f:
+
+with open(
+    "/Users/pokey/src/cursorless-vscode/src/test/suite/fixtures/recorded/tutorial/unit-2-basic-coding/script.json"
+) as f:
     script = json.load(f)
 
 print([process_tutorial_step(step) for step in script])
